49fdb6ccc7fe06e84a63c4c987bbdb9b769d89ed
Ermesson <ermessonlimadossantos@hotmail.com>
2023-01-03 02:21:14 -0300

    feat: ensure remoteAuthentication correct body

--- src/data/usecases/authentication/remote-authentication.ts
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -1,10 +1,11 @@
 import { HttpPostClient } from "@/data/protocols/http/http-post-client";
+import { AuthenticationParams } from "@/domain/usecases/authentication";
 
 export  class RemoteAuthentication {
     constructor(private readonly url: string,
         private readonly httpPostClient: HttpPostClient) { }
 
-    async auth(): Promise<void> {
-        await this.httpPostClient.postUrl({url:this.url})
+    async auth(params: AuthenticationParams): Promise<void> {
+        await this.httpPostClient.postUrl({url:this.url, body:params})
     }
 }
