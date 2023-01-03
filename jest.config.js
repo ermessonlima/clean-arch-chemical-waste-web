19193f0965e27116b9c9a2f08cf0647844d399ac
Ermesson Lima <ermessonlima@MacBook-Air-de-Ermesson.local>
2023-01-03 01:24:06 -0300

    feat: add authentication usecase

--- jest.config.js
+++ jest.config.js
@@ -11,5 +11,4 @@ module.exports = {
     transform: {
       '.+\\.(ts|tsx)$': 'ts-jest'
     },
- 
   }
--- src/a.spec.ts
+++
@@ -1,6 +0,0 @@
-describe( 'a', () => {
-    it( 'should work', () => {
-        expect( true ).toBe( true );
-    });
-    }
-)
