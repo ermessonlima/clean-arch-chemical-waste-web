c76abe09ed695040f93d0a561abfdb7dd931a233
Ermesson <ermessonlimadossantos@hotmail.com>
2023-01-04 00:24:41 -0300

    feat: ensure axiosHttp call axios with correct body

--- src/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -1,15 +1,31 @@
 import { faker } from "@faker-js/faker"
 import { AxiosHttpClient } from "./axios-http-client"
 import axios from 'axios'
+import { HttpPostParams } from "@/data/protocols/http"
+import { first } from "cypress/types/lodash"
 
 jest.mock('axios')
 const mockedAxios = axios as jest.Mocked<typeof axios>
 
+const makeSut = (): AxiosHttpClient => {
+    return new AxiosHttpClient()
+}
+
+const mockPostRequest = (): HttpPostParams<any> => ({
+    url: faker.internet.url(),
+    body:  {
+        name: faker.name.firstName(),
+        lastName : faker.name.lastName()
+    }
+})
+
 describe('AxiosHttpClient', () => {
-    test('Should call axios with correct URL', async () => {
-        const url = faker.internet.url()
-        const sut = new AxiosHttpClient()
-        await sut.post({ url })
-        expect(mockedAxios).toHaveBeenCalledWith(url)
+    test('Should call axios with correct values', async () => {
+        const request = mockPostRequest()
+        const sut = makeSut()
+        await sut.post(request)
+        expect(mockedAxios.post).toHaveBeenCalledWith(request.url, request.body)
     })
+
+ 
 })
