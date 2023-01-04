f64c3966f62a4812351c9c60f222edf9cb946611
Ermesson <ermessonlimadossantos@hotmail.com>
2023-01-03 23:28:41 -0300

    feat: ensure remoteAuthentication returns an account on success

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -2,7 +2,7 @@
 import { HttpPostClientSpy } from "@/data/test/mock-http-client"
 import { RemoteAuthentication } from "./remote-authentication"
 import { faker } from '@faker-js/faker';
-import { mockAuthentication } from "@/domain/test/mock-authentication";
+import { mockAccountModel, mockAuthentication } from "@/domain/test/mock-account";
 import { InvalidCredentialsError } from "@/domain/errors/invalid-credentials-error";
 import { HttpStatusCode } from "@/data/protocols/http/http-response";
 import { UnexpectedError } from "@/domain/errors/unexpected-error";
@@ -74,5 +74,17 @@ describe('RemoteAuthentication', () => {
         }
         const promise = sut.auth(mockAuthentication())
         await expect(promise).rejects.toThrow(new  UnexpectedError())
+    })
+
+
+    test('Should return an AncaountModel if HttpPostClient returns 200', async () => {
+        const { sut, httpPostClientSpy } = makeSut()
+        const httpResult = mockAccountModel()
+        httpPostClientSpy.response = {
+            statusCode: HttpStatusCode.ok,
+            body:  httpResult
+        }
+        const account = await  sut.auth(mockAuthentication())
+        expect(account).toEqual(httpResult)
     })
 })
